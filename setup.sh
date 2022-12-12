c152d3c5fbfafada3433b486b9e7616fdf7c9811
papin158 <papin158@mail.ru>
2022-12-12 17:26:23 +0200

    Add

--- setup.sh
+++ setup.sh
@@ -2,14 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"papin158@mail.ru\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-
-echo “\
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
